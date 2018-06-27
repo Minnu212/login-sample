6a43fe5593d62b946a75af237ae9ed905f3f83b1
Minhajuddin Quazi <quazi.minhaj88@gmail.com>
2018-06-27 13:25:08 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+ var App=()=>{
+    return (
+        <div>
+            <h1>Heyyyyy</h1>
+        </div>    
+    )
+}
+
+
+ReactDOM.render(<App/>,document.querySelector('#root'));
